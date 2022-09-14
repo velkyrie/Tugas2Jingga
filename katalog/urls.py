ce3199a1ad035dc973cdfa113963fe9a0f3a1220
jinggamentari <vladijm@gmail.com>
2022-09-14 23:56:23 +0700

    modified views, urls, html file

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,10 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from example_app.views import index
+from katalog.views import show_katalog
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_katalog, name='show_katalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = { 
+        'list_item': data_barang_katalog,
+        'nama': 'Vladi Jingga Mentari',
+        'student_id': '2106635631'
+    }
+    return render(request, "katalog.html", context)
