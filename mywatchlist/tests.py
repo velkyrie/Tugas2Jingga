527a5e39ad049b70a28193661d4f464be4716891
jinggamentari <vladijm@gmail.com>
2022-09-22 09:22:41 +0700

    added /html path

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestMywatchlist(TestCase):
+    def test_html(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+    
+    def test_json(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+
+    def test_xml(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -9,6 +9,7 @@ app_name = 'mywatchlist'
 
 urlpatterns = [
     path('', show_watchlist, name='show_watchlist'),
+    path('html/', show_watchlist, name='show_watchlist'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
