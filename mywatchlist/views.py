3aa7c9fca74d4de438ad74c33e804df438adab7a
jinggamentari <vladijm@gmail.com>
2022-09-22 11:52:17 +0700

    bonus done

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,10 +6,19 @@ from django.core import serializers
 # Create your views here.
 def show_watchlist(request):
     data_watchlist = MyWatchlist.objects.all()
+    is_watched = 0
+    not_watched = 0
+    for a in data_watchlist:
+        if a.watched:
+            is_watched += 1
+        else:
+            not_watched += 1
     context = { 
         'list_item': data_watchlist,
         'nama': 'Vladi Jingga Mentari',
-        'student_id': '2106635631'
+        'student_id': '2106635631',
+        'x': is_watched,
+        'y': not_watched,
     }
     return render(request, "mywatchlist.html", context)
 
